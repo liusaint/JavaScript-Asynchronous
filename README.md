389a2d84d7eaaaff70c335829c1012c1e4efb200
Runner <841766635@qq.com>
2017-05-15 00:07:45 +0800

    执行顺序的问题

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Javascript-Asynchronous
 Javascript 异步编程
 
-####这里记录关于Javascript异步编程的一些东西。
-####平时的思考和尝试，写过的demo，读过的书，总该留下痕迹。
+###### 这里记录关于Javascript异步编程的一些东西。
+###### 平时的思考和尝试，写过的demo，读过的书，总该留下痕迹。
--- promise/api.js
+++ promise/api.js
@@ -1,33 +1,77 @@
-var promise = new Promise(function(resolve, reject) {
+(function() {
+/* *注意这里的timeout值为0或10，执行的结果会不一样*/
+	// return;
+	var promise = new Promise(function(resolve, reject) {
+console.log('timer1_begin',new Date().getTime())
+		setTimeout(function() {
+			console.log('in',new Date().getTime());
+			if (Math.random() < 0.5) {
+				resolve(6)
+			} else {
+				reject(4);
+			}
 
-	setTimeout(function() {
+		}, 0);
 
-		if (Math.random() < 0.5) {
-			resolve(6)
-		} else {
-			reject(4);
-		}
-
-
-	}, 1000);
-
-})
-
-console.log(promise); //Promise { <pending> }
-setTimeout(function() {
-	console.log(promise); // Promise { 6 } 或 Promise { <rejected> 4 }
-
-	//resolved 之后注册的。
-	promise.then(function(value) {
-		console.log(value, 'resolved0');
-	}, function(value) {
-		console.log(value, 'rejected0');
 	})
-}, 1000);
 
-//resolved 之前注册的。
-promise.then(function(value) {
-	console.log(value, 'resolved');
-}, function(value) {
-	console.log(value, 'rejected');
-})
+	console.log(promise); //Promise { <pending> }
+	console.log('timer2_begin',new Date().getTime())
+	setTimeout(function() {
+		console.log('out',new Date().getTime())
+		// console.log(promise); // Promise { 6 } 或 Promise { <rejected> 4 }
+
+		//resolved 之后注册的。
+		// promise.then(function(value) {
+		// 	console.log(value, 'resolved0');
+		// }, function(value) {
+		// 	console.log(value, 'rejected0');
+		// })
+
+
+	}, 0);
+
+	//resolved 之前注册的。
+	promise.then(function(value) {
+		console.log(value, 'resolved',new Date().getTime());
+	}, function(value) {
+		console.log(value, 'rejected',new Date().getTime());
+	})
+
+})();
+
+
+/*resolve中传递promise,p2的状态由p1决定
+ */
+
+(function() {
+	return;
+	var p1 = new Promise(function(resolve, reject) {
+
+		setTimeout(function() {
+
+			if (Math.random() < 0.5) {
+				resolve(6)
+			} else {
+				reject(4);
+			}
+
+		}, 10);
+
+	})
+
+	var p2 = new Promise(function(resolve, reject) {
+		resolve(p1);
+	})
+
+	p2.then(function(value) {
+		console.log('p2 yes');
+	}, function(value) {
+		console.log('p2 no');
+	})
+
+	console.log(p1, p2, 1)
+	setTimeout(function() {
+		console.log(p1, p2, 2)
+	}, 10)
+})();
